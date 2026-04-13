68c3f9ac95f99a720ba80d3330bb85ae2e16b58a
Adeeba <adeeba.shoaib@dataoids.com>
2026-04-13 13:01:08 +0530

    Lesson 2: Implement path params and query params

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from typing import Union
+
+from fastapi import FastAPI, Path, HTTPException, Query
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -14,5 +16,27 @@ def home():
     return f"<h1>{users[0]['name']}</h1>"
 
 @app.get("/api/users")
-def get_user():
+def get_users():
     return users
+
+# Path Params
+@app.get("/users/{id}")
+def get_specific_user(id: int = Path(..., description= 'ID of the user in the DB', examples = 1)):
+    for user in users:
+        if user["id"] == id:
+            return user
+    
+    raise HTTPException(status_code=404, detail="User not found")
+
+@app.get("/sort")
+def sort_users(sort_by :Union[int, str]= Query(..., description = 'Sort on the basis of id, name'), order: str= Query('asc', description= 'Sort in asc or desc order.')):
+    if sort_by not in ['id', 'name']:
+        raise HTTPException(status_code=400, detail = 'Invalid selection of value.')
+    
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code= 404, detail = 'Invalid order select.')
+    
+    sort_order = True if order == 'desc' else False
+    sorted_data = sorted(users, key=lambda user: user[sort_by], reverse = sort_order)
+    
+    return sorted_data
